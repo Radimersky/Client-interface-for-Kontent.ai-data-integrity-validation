ffe4056f47abd8f69dbd8e761618c3d26350d771
Marek Radiměřský <xradimer@fi.muni.cz>
2022-05-19 17:08:48 +0200

    Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import './App.css';
-import { Route, Link as RouterLink, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import BlockchainVariants from './pages/BlockchainVariants';
 import UnprocessedVariants from './pages/UnprocessedVariants';
-import { Button, CssBaseline } from '@mui/material';
+import { CssBaseline } from '@mui/material';
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <RouterLink to="/blockchain">
-        <Button variant="contained">Blockchain</Button>
-      </RouterLink>
       <Routes>
         <Route path="/" element={<UnprocessedVariants />} />
       </Routes>
--- src/pages/UnprocessedVariants.tsx
+++ src/pages/UnprocessedVariants.tsx
@@ -1,5 +1,13 @@
-import { Paper } from '@mui/material';
+import { Button, Container, Paper } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
 
-const UnprocessedVariants = () => <Paper>okcackasockacoackosckaockaokcaokcascoako</Paper>;
+const UnprocessedVariants = () => (
+  <Container>
+    <Paper>okcackasockacoackosckaockaokcaokcascoako</Paper>
+    <RouterLink to="/blockchain">
+      <Button variant="contained">Blockchain</Button>
+    </RouterLink>
+  </Container>
+);
 
 export default UnprocessedVariants;

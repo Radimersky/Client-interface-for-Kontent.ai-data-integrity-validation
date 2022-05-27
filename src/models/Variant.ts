bba2ce6060720f9ecf14a5c8c38607449bb11c60
Marek Radiměřský <xradimer@fi.muni.cz>
2022-05-27 18:04:23 +0200

    Finish implementation of server model

--- src/models/Variant.ts
+++ src/models/Variant.ts
@@ -4,19 +4,51 @@ import { ProgramAccount, IdlTypes, Idl } from '@project-serum/anchor';
 import { IdlAccountDef } from '@project-serum/anchor/dist/cjs/idl';
 // eslint-disable-next-line no-unused-vars
 import { TypeDef } from '@project-serum/anchor/dist/cjs/program/namespace/types';
+import { PublicKey } from '@solana/web3.js';
+import dayjs from 'dayjs';
 
 export type Variant = {
   readonly projectId: String;
+  readonly itemId: String;
+  readonly variantId: String;
+  readonly variantHash: String;
+  readonly variantHashSignature: String;
+  readonly publicKey: String;
+  readonly shortAuthor: String;
+  readonly author: String;
+  readonly lastModified: String;
+  readonly accountCreated: String;
 };
 
 export type ServerVariant = {
   readonly projectId: String;
+  readonly itemId: String;
+  readonly variantId: String;
+  readonly variantHash: String;
+  readonly variantHashSignature: String;
+  readonly publicKey: PublicKey;
+  readonly author: PublicKey;
+  readonly lastModified: number;
+  readonly accountCreated: number;
 };
 
 export const Variant = {
   fromServerModel(serverModel: ProgramAccount<TypeDef<IdlAccountDef, IdlTypes<Idl>>>): Variant {
+    const castedModel = serverModel as ProgramAccount<ServerVariant>;
+
+    const author = castedModel.publicKey.toBase58();
+
     return {
-      projectId: serverModel.account.projectId as String
+      projectId: castedModel.account.projectId,
+      itemId: castedModel.account.itemId,
+      variantId: castedModel.account.variantId,
+      variantHash: castedModel.account.variantHash,
+      variantHashSignature: castedModel.account.variantHashSignature,
+      publicKey: castedModel.publicKey.toBase58(),
+      author: author,
+      shortAuthor: author.slice(0, 4) + '..' + author.slice(-4),
+      lastModified: dayjs.unix(castedModel.account.lastModified).format('YYYY-MM-DDTHH:mmZ'),
+      accountCreated: dayjs.unix(castedModel.account.accountCreated).format('YYYY-MM-DDTHH:mmZ')
     };
   }
 };

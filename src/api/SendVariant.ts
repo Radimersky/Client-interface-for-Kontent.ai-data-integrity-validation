277006dd090ae63066cc90e7e9d153bc7cf4855e
Marek Radiměřský <xradimer@fi.muni.cz>
2022-05-29 19:53:14 +0200

    Implement save variant api call

--- src/api/SendVariant.ts
+++ src/api/SendVariant.ts
@@ -1,13 +1,32 @@
 // eslint-disable-next-line no-unused-vars
+import { AnchorProvider, BN, web3 } from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor/dist/cjs/program';
 
-export const fetchVariants = async (program: Program, filters = []) => {
-  return await program.account.variant.all(filters);
+// Stores variant on blockchain
+export const sendVariant = async (program: Program, provider: AnchorProvider) => {
+  // Create key pair for new variant account
+  const variant = web3.Keypair.generate()
+
+  await program.methods.saveVariant(
+    'bb1439d5-4ee2-4895-a4e4-5b0d9d8c754e',
+    'ad1439d5-4ee2-4895-a4e4-5b0d9d8c754e',
+    'bd1439d5-4ee2-4895-a4e4-5b0d9d8c754e',
+    '0x7368b03bea99c5525aa7a9ba0b121fc381a4134f90d0f1b4f436266ad0f2b43b',
+    '0x7368b03bea99c5525aa7a9ba0b121fc381a4134f90d0f1b4f436266ad0f2b43b',
+    new BN(1551041404),
+    {
+      accounts: {
+        variant: variant.publicKey,
+        author: provider.wallet.publicKey,
+        systemProgram: web3.SystemProgram.programId
+      },
+      signers: [variant]
+    }
+  );
+
+  const variantAccount = await program.account.variant.fetch(variant.publicKey);
+  console.log(variantAccount);
+
+  return variantAccount;
 };
 
-export const authorFilter = (authorBase58PublicKey: any) => ({
-  memcmp: {
-    offset: 8, // Discriminator.
-    bytes: authorBase58PublicKey
-  }
-});
--- src/utils/useWorkspace.tsx
+++ src/utils/useWorkspace.tsx
@@ -11,7 +11,6 @@ interface IWorkspaceProps {
 }
 
 const preflightCommitment = 'processed';
-const commitment = 'processed';
 const programID = new PublicKey(idl.metadata.address);
 const network = 'http://127.0.0.1:8899';
 
@@ -19,11 +18,9 @@ const useWorkspace = (): IWorkspaceProps => {
   const wallet = useAnchorWallet();
 
   const connection = new Connection(network, preflightCommitment);
-  const provider = new AnchorProvider(connection, wallet as Wallet, {
-    preflightCommitment: preflightCommitment,
-    commitment: commitment
-  });
-  const program = new Program(idl as any, programID, provider);
+  const provider = new AnchorProvider(connection, wallet as Wallet, {});
+  const program = new Program(idl as Idl, programID, provider);
+
   return {
     //wallet,
     //connection,

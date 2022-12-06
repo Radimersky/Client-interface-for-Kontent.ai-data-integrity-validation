dea32f5ea4c4836d0ce462fdbd95b1e527aa356d
Marek Radimersky <radimersky@protonmail.com>
2022-12-06 22:29:06 +0100

    Add webpack config override

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,13 @@
 module.exports = function override(config, env) {
 	console.log('Config overriden');
+	const fallback = config.resolve.fallback || {};
+
+	Object.assign(fallback, {
+		crypto: false,
+		stream: false,
+	})
+
+    config.resolve.fallback = fallback;
+	
 	return config;
 };
